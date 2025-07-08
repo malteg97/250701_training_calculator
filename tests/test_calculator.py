0691eded18b8c65ba55ec4a2b0a447f693c3c211
malteg97 <malte.gleim@live.de>
2025-07-08 16:20:30 +0200

    Nachrichten ergänzt

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,13 +4,13 @@ from testproject_calculator import calculator
 class TestCalculator:
 
     def test_add(self):
-        assert 4 == calculator.add(1, 3)
+        assert 4 == calculator.add(1, 3), f"Addition fehlgeschlagen"
 
     def test_multiply(self):
-        assert 15 == calculator.multiply(5, 3)
+        assert 15 == calculator.multiply(5, 3), f"Multiplikation fehlgeschlagen"
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(5, 3)
+        assert 2 == calculator.subtract(5, 3), f"Subtraktion fehlgeschlagen"
 
     def test_divide(self):
-        assert 2 == calculator.divide(6, 3)
+        assert 2 == calculator.divide(6, 3), f"Division fehlgeschlagen"
