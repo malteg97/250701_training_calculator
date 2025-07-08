d1210e991fb266b9839e3d2e25dfc1edba09190f
malteg97 <malte.gleim@live.de>
2025-07-08 11:58:25 +0200

    Start

--- testproject_calculator/calculator.py
+++ testproject_calculator/calculator.py
@@ -8,19 +8,6 @@ def add(num1, num2):
     print(f"{num1} + {num2} = {result}")
     return result
 
-def subtract(num1, num2):
-    """Subtract two numbers
-    """
-    result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
-    return result
-
-def multiply(num1, num2):
-    """Multiply two numbers
-    """
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-    return result
 
 def is_number(value):
     try:
