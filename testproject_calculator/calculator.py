2f4f8bd93220da517d757e0678f7bba42e69c7a0
malteg97 <malte.gleim@live.de>
2025-07-08 11:53:16 +0200

    Multiplikation implementiert

--- testproject_calculator/calculator.py
+++ testproject_calculator/calculator.py
@@ -15,6 +15,13 @@ def subtract(num1, num2):
     print(f"{num1} - {num2} = {result}")
     return result
 
+def multiply(num1, num2):
+    """Multiply two numbers
+    """
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+    return result
+
 def is_number(value):
     try:
         float(value)
@@ -44,6 +51,7 @@ def switch_operator(operator):
     switcher = {
         "+": add,
         "-": subtract,
+        "*": multiply,
         "": None
     }
     return switcher.get(operator, "invalid")
