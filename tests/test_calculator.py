4f0ed66e76e10e2f916c794571b0cd663e76a8c4
malteg97 <malte.gleim@live.de>
2025-07-10 11:26:14 +0200

    Tests hinzu

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,6 +5,15 @@ class TestCalculator:
 
     def test_add(self):
         assert 4 == calculator.add(1, 3)
+    
+    def test_multiply(self):
+        assert 6 == calculator.multiply(2, 3)
 
+    def test_subtract(self):
+        assert 1 == calculator.subtract(3, 2)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(6, 3)
+        assert None is calculator.divide(6, 0)
 
 
